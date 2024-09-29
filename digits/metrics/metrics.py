7aae77acc88c1170df25a60fa0896aa02060b6d8
Luiz Fernando de Lima <luizfernandofpl@gmail.com>
2024-09-29 13:06:40 -0300

    Implements confusion matrix

--- digits/metrics/metrics.py
+++ digits/metrics/metrics.py
@@ -1,4 +1,7 @@
 import numpy as np
+import seaborn as sns
+from matplotlib import pyplot as plt
+from sklearn.metrics import confusion_matrix
 
 
 def compute_acc(Y, Y_pred):
@@ -8,3 +11,12 @@ def compute_acc(Y, Y_pred):
         if Y_pred[idx] == Y[idx]:
             acc += 1
     return acc/sample_size
+
+def confusion(Y, Y_pred):
+    cm = confusion_matrix(Y, Y_pred)
+
+    sns.heatmap(cm, annot=True, fmt='d', cmap='Blues', xticklabels=[-1, 1], yticklabels=[-1, 1])
+    plt.xlabel('Predicted Labels')
+    plt.ylabel('True Labels')
+    plt.title('Confusion Matrix')
+    plt.show()
